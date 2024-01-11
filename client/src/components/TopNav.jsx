1c57f77f3d9035bc56ff8fb705e73f543c16403a
Abdullahi-auwalu <awaluabdullahi07@gmail.com>
2024-01-11 14:20:51 +0100

    add sideNav

--- client/src/components/TopNav.jsx
+++ client/src/components/TopNav.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   AiOutlineMenu,
   AiOutlineSearch,
@@ -5,15 +6,27 @@ import {
   AiFillTag,
 } from "react-icons/ai";
 import { BsFillCartFill, BsPerson } from "react-icons/bs";
+import { TbTruckReturn } from "react-icons/tb";
+import { FaUserFriends, FaGoogleWallet } from "react-icons/fa";
+import { MdOutlineFavoriteBorder } from "react-icons/md";
+import { IoMdHelp } from "react-icons/io";
 
 const TopNav = () => {
+  const [sideNav, setSideNav] = useState(false);
+  console.log(sideNav);
   return (
     <div className="max-w-[1520px] flex justify-between p-4  mx-auto items-center ">
       <div className="flex items-center">
-        <div className="cursor-pointer">
+        <div onClick={() => setSideNav(!sideNav)} className="cursor-pointer">
           <AiOutlineMenu size={25} />
         </div>
-        <h1 className="text-2xl sm:text-3xl lg:text-4xl p-4">Epicure</h1>
+        <h1
+          className="text-2xl sm:text-3xl lg:text-4xl p-4
+          text-orange-700 font-bold "
+        >
+          Epi<span className="text-black font-medium">cure</span>
+        </h1>
+
         <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
           <p className="bg-orange-700 text-white rounded-full p-2 text-bold">
             Free
@@ -33,6 +46,70 @@ const TopNav = () => {
         <BsFillCartFill />
         Cart
       </button>
+      {sideNav ? (
+        <div
+          className="bg-black/60 fixed w-full h-screen left-0 top-0 z-10"
+          onClick={() => setSideNav(!sideNav)}
+        ></div>
+      ) : (
+        ""
+      )}
+
+      <div
+        className={
+          sideNav
+            ? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300 "
+            : "fixed top-0 left-[-999px] w-[300px] h-screen bg-white z-10  duration-300 "
+        }
+      >
+        <AiOutlineClose
+          onClick={() => setSideNav(!sideNav)}
+          size={25}
+          className="absolute right-4 top-4 cursor-pointer"
+        />
+        <h2 className="text-orange-700 font-bold  text-2xl p-4">
+          Epi<span className="text-black font-medium">cure</span>
+        </h2>
+        <nav>
+          <ul className="flex flex-col p-4 text-gray-900">
+            <li className="text-xl py-4 flex">
+              <BsPerson
+                size={25}
+                className="mr-4 text-white bg-black rounded-full"
+              />
+              My Account
+            </li>
+            <li className="text-xl py-4 flex">
+              <TbTruckReturn
+                size={25}
+                className="mr-4 text-white bg-black rounded-full"
+              />
+              Delivery
+            </li>
+            <li className="text-xl py-4 flex">
+              <MdOutlineFavoriteBorder
+                size={25}
+                className="mr-4 text-white bg-black rounded-full"
+              />
+              My Favourite
+            </li>
+            <li className="text-xl py-4 flex">
+              <FaGoogleWallet
+                size={25}
+                className="mr-4 text-white bg-black rounded-full"
+              />
+              My Wallet
+            </li>
+            <li className="text-xl py-4 flex">
+              <IoMdHelp
+                size={25}
+                className="mr-4 text-white bg-black rounded-full"
+              />
+              Help
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
